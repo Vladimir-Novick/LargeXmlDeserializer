eded73a93231db186a17a6a7420dd91eef9ed9e3
Vladimir Novick <vlad.novick@gmail.com>
2017-10-10 10:42:17 -0400

    Simple Xml Deserialization

---
+++ SimpleXmlDeserializator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XmlUtils
+{
+    /// <summary>
+    ///  Use:
+    ///  
+    ///       SimpleXmlDeserializator sertalizator = new SimpleXmlDeserializator();
+    ///
+    ///       rootAreas = sertalizator.DeserializeFile<RootAreas>(CrawlerConfig.GetConfigData.TargetFolder + "AREAS.xml");
+    /// 
+    /// </summary>
+    public class SimpleXmlDeserializator
+    {
+        public T Deserialize<T>(String strXMLAreas)
+        {
+            T ret;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (StringReader rdr = new StringReader(strXMLAreas))
+            {
+                ret = (T)serializer.Deserialize(rdr);
+            }
+            serializer = null;
+            return ret;
+        }
+
+
+        public T DeserializeFile<T>(String strXMLFileName)
+        {
+            string strXMLAreas = File.ReadAllText(strXMLFileName, System.Text.Encoding.UTF8);
+            T ret =  Deserialize<T>(strXMLAreas);
+            strXMLAreas = null;
+            return ret;
+        }
+
+
+    }
+}
